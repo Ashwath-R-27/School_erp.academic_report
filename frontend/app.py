4a3b420c111bdc19b26b9d37dd86acefb18a17da
JCKawin <jckawin2007@gmail.com>
2026-05-26 09:18:32 +0530

    corrected slilly mistakes

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def first():
     return render_template("home.html")
 
+
 @app.route("/HSC_2026")
 def hscmark():
     return render_template("hscmarkpg.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
